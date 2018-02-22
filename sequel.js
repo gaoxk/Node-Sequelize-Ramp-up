29476bcda963f795e97f39ac9ec00ac53c3f8ec9
gaoxk <kristy.crazy4pandas.gao@gmail.com>
2018-02-22 01:59:18 -0500

    insert and selecting data with sequelize

--- sequel.js
+++ sequel.js
@@ -1,10 +1,31 @@
 var Sequelize = require('sequelize');
 //make things caps to indicate constructive func
-var con = new Sequelize('name of db', 'root', ''); 
-//conect to db, (db name, username, password) <----- sequelize assumes youre using mysql
-//usually youll need to speciify the db stuff with an object
+var con = new Sequelize('dbnode', 'root', '',{
+	host: 'localhost',
+	dialect: 'mysql',
+	pool:{
+		max: 5,
+	    min: 0,
+	    acquire: 30000,
+	    idle: 1000
+	}
+}); 
 
-var Artivle = con.define('article', {//mekign a new table
+var Article = con.define('article', {//making a new table
 	title: Sequelize.STRING,
 	body: Sequelize.TEXT
+});
+
+//con.sync();//connect to database 
+con.sync().then(function(){
+	
+	/* inserting info to the table
+	Article.create({
+		title: 'my first title!',
+		body: 'my first article! Well aint this just fun'
+	});*/
+
+	Article.findById(1).then(function(article){
+		console.log(article.dataValues);
+	});
 });
