d345bc4219815ae96cd4466cfd083033e5a5c703
gaoxk <kristy.crazy4pandas.gao@gmail.com>
2018-02-22 19:08:07 -0500

    finished learning modules

--- sequel.js
+++ sequel.js
@@ -12,20 +12,44 @@ var con = new Sequelize('dbnode', 'root', '',{
 }); 
 
 var Article = con.define('article', {//making a new table
-	title: Sequelize.STRING,
-	body: Sequelize.TEXT
+	slug:{
+		type: Sequelize.STRING,
+		primaryKey: true
+	},
+	title: {
+		type: Sequelize.STRING,
+		unique: true, // all titles are unique
+		//if you try to insert something thats already existing, 
+		//sq will throw an error
+		allowNull: false
+	},
+
+	//body: Sequelize.TEXT, same as the one below
+	body: {
+		type: Sequelize.TEXT,
+		//defaultValue: 'default time!' doesnt work w text + windows
+	}
+}, {
+	timestamps: false
 });
 
+//sync creates tables and data, doesnt update
+//migrations basically update without manually deleting tables
+
 //con.sync();//connect to database 
-con.sync().then(function(){
-	
+
+con.sync({
+	force: true,//clears out old tables of same name
+	logging: console.log
+}).then(function(){	
 	/* inserting info to the table
 	Article.create({
 		title: 'my first title!',
 		body: 'my first article! Well aint this just fun'
 	});*/
 
+	/* selecting from table
 	Article.findById(1).then(function(article){
 		console.log(article.dataValues);
-	});
-});
+	}); */
+}); 
