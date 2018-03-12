eaf20e996f843531ade62143297980f8ddaed5d1
gaoxk <kristy.crazy4pandas.gao@gmail.com>
2018-03-12 11:13:40 -0400

    Added static serving - working on data movement demo

--- insert.js
+++ insert.js
@@ -2,6 +2,7 @@ var Sequelize = require('sequelize');
 //for the front end
 var express = require('express');
 var app = express();
+var path = require('path');
 
 //make things caps to indicate constructive func
 var con = new Sequelize('dbnode', 'root', '',{
@@ -15,19 +16,13 @@ var con = new Sequelize('dbnode', 'root', '',{
 	}
 }); 
 
-var Article = con.define('articleTable', {//making a new table
-	title: {
+var TextChange = con.define('textChangeTable', {//making a new table
+	identifier: {
 		type: Sequelize.STRING,
-		unique: true, // all titles are unique
+		unique: false, // all titles are unique
 		//if you try to insert something thats already existing, 
 		//sq will throw an error
 		allowNull: false,
-		validate: {
-			len: {//customize your validation
-				args: [10,69],
-				msg: 'The title should be no more than 69 char and no less than 10 char'
-			}
-		}
 	},
 
 	//body: Sequelize.TEXT, same as the one below
@@ -62,15 +57,8 @@ con
 	})
 	.then(function(){	//connection is done
 		
-		
-		//fails validaion 
-	 	/*return Article.create({
-			title: 'my first Article!!!!!!!!!!!!!!!1',
-			body: 'My first article! Well aint this just fun'
-		}); */	
-
-		Article.create({
-			title: 'Some prersistent title',
+		TextChange.create({
+			identifier: 'Some prersistent title',
 			body: 'Uses .create'
 		}).then(function (insertedReord){
 			//promise
@@ -81,27 +69,6 @@ con
 			display = insertedReord.dataValues;
 
 		});
-
-		var articleInstance = Article.build({
-			title: 'Some non prersistent title',
-			body: 'Uses .build'
-		})
-		articleInstance.save();
- 
-		//say you retrieved an article from a user
-		var req = {
-			here: {
-				title: "I'm a request!",
-				body: "Oh yeah!"
-			}
-		};
-
-		Article.create(req.here);
-
-		/* selecting from table
-		Article.findById(1).then(function(article){
-			console.log(article.dataValues);
-		}); */
 	})
 	.catch(function(error){
 		console.log("oopsies: " + error);
@@ -109,7 +76,18 @@ con
 
 
 app.get('/', function(req, resp){
-	resp.send(display);
+	var filename = path.basename('public/index.html');
+	resp.send(filename);
 });
 
-app.listen(1337);
+
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
